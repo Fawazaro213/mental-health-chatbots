23fb8e5bcdf4048b9998268d70bbd7b149d3ac97
Fawazaro213 <arofawaz123@gmail.com>
2025-07-21 13:47:13 +0100

    installation

--- static/js/sidebar.js
+++ static/js/sidebar.js
@@ -1,22 +1,3 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//   const sidebar = document.querySelector(".sidebar");
-//   const sidebarToggler = document.querySelector(".sidebar-toggler");
-
-//   if (sidebarToggler) {
-//     sidebarToggler.addEventListener("click", () => {
-//       sidebar.classList.toggle("collapsed");
-//       // Update icon
-//       const icon = sidebarToggler.querySelector("span");
-//       if (sidebar.classList.contains("collapsed")) {
-//         icon.textContent = "chevron_right";
-//       } else {
-//         icon.textContent = "chevron_left";
-//       }
-//     });
-//   }
-// });
-
-
 document.addEventListener("DOMContentLoaded", function () {
   const sidebar = document.querySelector(".sidebar");
   const sidebarToggler = document.querySelector(".sidebar-toggler");
